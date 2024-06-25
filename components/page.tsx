53c9b56e84c499489f89c00b063ccc14efe4bc4b
EnricoNipper <enrico.vicenzo@hotmail.com>
2024-06-24 21:37:44 -0300

    2.3

--- components/page.tsx
+++ components/page.tsx
@@ -15,13 +15,13 @@ const Project = () => {
         <h3 className='text-center'> 
           <span className="text-3xl  font-semibold text-[50px]   text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500 ">{t("h1")}</span> {/* Título da seção com tradução */}
         </h3>
-        <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">  {/* // Descrição do portfólio  */}  
+        <p className="text-md text-center  p-2 leading-8 text-gray-800 dark:text-gray-200">  {/* // Descrição do portfólio  */}  
           {t("p1")} {/* Descrição do portfólio com tradução */}
           <span className="text-teal-500"> {t("span1")} </span> {/*  Destaque na descrição com tradução */}
           {t("p2")} {/* Descrição do portfólio com tradução  */} 
           <span className="text-teal-500"> {t("span2")} </span> {/*  Destaque na descrição com tradução */} 
         </p>
-        <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200"> {/* Descrição do portfólio  */} 
+        <p className="text-center  text-md py-2 leading-8 text-gray-800 dark:text-gray-200"> {/* Descrição do portfólio  */} 
           {t("p3")} {/* Descrição do portfólio com tradução */} 
         </p>
       </div>
